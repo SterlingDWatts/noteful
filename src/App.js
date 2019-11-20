70c95d2c20af73f8e972765c72543eda9b2b5102
Sterling Watts <SterlingDWatts@gmail.com>
2019-11-20 12:43:52 -0800

    Created header and noteful_app section on App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import STORE from './dummy-store';
 import './App.css';
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            folders: STORE.folders,
+            notes: STORE.notes
+        }
+    }
+
     render() {
         return (
             <main>
-
+                <header>
+                    <h1>Noteful</h1>
+                </header>
+                <section className="noteful_app">
+                    
+                </section>
             </main>
         );
     }
