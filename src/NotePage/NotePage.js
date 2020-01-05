41b8ac1678d91fe49f324c428394ba5da7e7910e
Sterling Watts <SterlingDWatts@gmail.com>
2020-01-05 10:56:25 -0800

    Fix NotePage Component if ID doesn't exist
    * Add renderNote(note) and renderNoNote functions
    * Add const notepage that conditionally renders one of the above
    functions

--- src/NotePage/NotePage.js
+++ src/NotePage/NotePage.js
@@ -17,34 +17,63 @@ class NotePage extends Component {
         this.context.deleteNote(this.props.match.params.noteId);
     }
 
-    render() {
-        const note = this.context.notes.find(note => note.id === this.props.match.params.noteId);
+    renderNote(note) {
         const folder = this.context.folders.find(folder => folder.id === note.folderId);
         return (
-            <NoteError>
-                <div className="note_page">
-                    <div className="back_to_folders_nav">
-                        <div className="go_back__box">
-                            <button 
-                                className="go_back"
-                                onClick={this.handleGoBack}
-                            >
-                                Go Back
-                            </button>
-                        </div>
-                        <h2>{folder && folder.name}</h2>
-                    </div>
-                    <div className="note_box__box">
-                        <ul>{note && (<NoteBox note={note} showDeleteButton={false} />)}</ul>
-                        <p>{note && note.content}</p>
+            <div className="note_page">
+                <div className="back_to_folders_nav">
+                    <div className="go_back__box">
                         <button 
-                            className='delete_note__button'
-                            onClick={this.handleDeleteNote}
+                            className="go_back"
+                            onClick={this.handleGoBack}
                         >
-                            Delete Note
+                            Go Back
+                        </button>
+                    </div>
+                    <h2>{folder && folder.name}</h2>
+                </div>
+                <div className="note_box__box">
+                    <ul>{note && (<NoteBox note={note} showDeleteButton={false} />)}</ul>
+                    <p>{note && note.content}</p>
+                    <button 
+                        className='delete_note__button'
+                        onClick={this.handleDeleteNote}
+                    >
+                        Delete Note
+                    </button>
+                </div>
+            </div>
+        );
+    }
+
+    renderNoNote() {
+        return (
+            <div className='note_not_found'>
+                <div className="back_to_folders_nav">
+                    <div className="go_back__box">
+                        <button 
+                            className="go_back"
+                            onClick={this.handleGoBack}
+                        >
+                            Go Back
                         </button>
                     </div>
                 </div>
+                <div className="note_box__box">
+                    <h2>Note Not Found</h2>
+                </div>
+            </div>
+        )
+    }
+
+    render() {
+        const note = this.context.notes.find(note => note.id === this.props.match.params.noteId)
+        const notepage = note
+            ? this.renderNote(note)
+            : this.renderNoNote();
+        return (
+            <NoteError>
+                { notepage }
             </NoteError>
         );
     }
